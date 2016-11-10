48e2cf304bb170d02fe3f06e041e39b2b2710321
Maddie <mk186442@falmouth.ac.uk>
2016-11-10 09:43:34 +0000

    Added createBuffers function for particle effect

--- COMP220/COMP220_Portfolio/ParticleEffectManager.cpp
+++ COMP220/COMP220_Portfolio/ParticleEffectManager.cpp
@@ -10,3 +10,30 @@ ParticleEffectManager::ParticleEffectManager()
 ParticleEffectManager::~ParticleEffectManager()
 {
 }
+
+void ParticleEffectManager::createBuffers()
+{
+	// VBO containing the 4 vertices of the particles.
+	static const GLfloat g_vertex_buffer_data[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
+	};
+
+	glGenBuffers(1, &billboard_vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	// The VBO containing the positions and sizes of the particles
+	glGenBuffers(1, &particles_position_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+	// Initialize with empty (NULL) buffer : it will be updated later, each frame.
+	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW);
+
+	// The VBO containing the colors of the particles
+	glGenBuffers(1, &particles_color_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
+	// Initialize with empty (NULL) buffer : it will be updated later, each frame.
+	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
+}
--- COMP220/COMP220_Portfolio/ParticleEffectManager.h
+++ COMP220/COMP220_Portfolio/ParticleEffectManager.h
@@ -2,7 +2,16 @@
 class ParticleEffectManager
 {
 public:
+	//! Constructor
 	ParticleEffectManager();
+	//! Destructor
 	~ParticleEffectManager();
+
+	//! Creates particle effects buffers
+	void createBuffers();
+
+private: 
+	GLuint billboard_vertex_buffer, particles_color_buffer, particles_position_buffer;
+	int MaxParticles = 10;
 };
 
