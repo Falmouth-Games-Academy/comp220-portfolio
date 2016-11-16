5af0a7e67885f025220bdb150fc11b1aebc176e9
Alastair Rayner <alli0@me.com>
2016-11-16 18:40:33 +0000

    Cleaned up Terrain code and floor bugs

--- Portfolio/COMP220/04_Cube/Terrain.cpp
+++ Portfolio/COMP220/04_Cube/Terrain.cpp
@@ -32,60 +32,36 @@ void Terrain::generateChunk(Mesh& mesh)
 
 	// The grounds colour Variable
 	glm::vec3 colour = glm::vec3(0, 0, 0);
-	int x = 0;
-	for (x; x < chunkSize; x++)
+
+	for (int x = 0; x < chunkSize; x++)
 	{
 		for (int z = 0; z < chunkSize; z++)
 		{
 			double perlinResult = perlinNoise.noise((x / noiseAmplification), (z / noiseAmplification), y);
-
-			// Forward declarations
 			double lastPerlinResult = perlinResult;
-			glm::vec3 lastA(0,0,0);
-			glm::vec3 lastB(0,0,0);
-			glm::vec3 lastC(0,0,0);
-			glm::vec3 lastD(0,0,0);
-			glm::vec3 lastE(0,0,0);
-			glm::vec3 lastF(0,0,0);
-			glm::vec3 lastG(0,0,0);
-			glm::vec3 lastH(0,0,0);
+
 			//Normalize values
 			perlinResult = (char)((perlinResult - noiseMin) * (255 / (noiseMax - noiseMin)));;
 
 			// Cube Colour
 			if (perlinResult > 0)
-			{
-				colour = glm::vec3(perlinResult / 100, perlinResult / 50, perlinResult / 700); // Grassy texture
-			}
+				colour = glm::vec3(perlinResult / 100, perlinResult / 50, perlinResult / 700);
 			else
-			{
 				colour = glm::vec3(-perlinResult / 7, -perlinResult / 10, perlinResult / 70);
-			}
-
-
+			
 			// Reduce the incline of the slope if it is below 0
-			if (perlinResult < lastPerlinResult )
+			if (perlinResult <= lastPerlinResult )
 			{
+				glm::vec3 a(x - SquareSize, perlinResult * 0.1 + SquareSize, z + SquareSize);
+				glm::vec3 b(x + SquareSize, perlinResult * 0.1 + SquareSize, z + SquareSize);
+				glm::vec3 c(x + SquareSize, perlinResult * 0.1 + SquareSize, z - SquareSize);
+				glm::vec3 d(x - SquareSize, perlinResult * 0.1 + SquareSize, z - SquareSize);
+				glm::vec3 e(x - SquareSize, perlinResult * 0.1 - SquareSize, z + SquareSize);
+				glm::vec3 f(x - SquareSize, perlinResult * 0.1 - SquareSize, z - SquareSize);
+				glm::vec3 g(x + SquareSize, perlinResult * 0.1 - SquareSize, z - SquareSize);
+				glm::vec3 h(x + SquareSize, perlinResult * 0.1 - SquareSize , z + SquareSize);
 				
-				glm::vec3 a(x - SquareSize, 0 + perlinResult * 0.1, z + SquareSize);
-				glm::vec3 b(x + SquareSize, 0 + perlinResult * 0.1, z + SquareSize);
-				glm::vec3 c(x + SquareSize, 0 + perlinResult * 0.1, z - SquareSize);
-				glm::vec3 d(x - SquareSize, 0 + perlinResult * 0.1, z - SquareSize);
-				glm::vec3 e(x - SquareSize, 0 + perlinResult * 0.1, z + SquareSize);
-				glm::vec3 f(x - SquareSize, 0 + perlinResult * 0.1, z - SquareSize);
-				glm::vec3 g(x + SquareSize, 0 + perlinResult * 0.1, z - SquareSize);
-				glm::vec3 h(x + SquareSize, 0 + perlinResult * 0.1, z + SquareSize);
-
 				mesh.addCube(a, b, c, d, e, f, g, h, colour);
-
-				glm::vec3 lastA(a);
-				glm::vec3 lastB(b);
-				glm::vec3 lastC(c);
-				glm::vec3 lastD(d);
-				glm::vec3 lastE(e);
-				glm::vec3 lastF(f);
-				glm::vec3 lastG(g);
-				glm::vec3 lastH(h);
 			}
 			else if (perlinResult > lastPerlinResult)
 			{
